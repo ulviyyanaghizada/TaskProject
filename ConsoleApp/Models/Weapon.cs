2859ddc84a9b2404c693292c5cae59e6b635e701
ca r215.03 <ca.r215.03@ca.local>
2022-12-13 01:44:02 -0800

    task project

---
+++ ConsoleApp/Models/Weapon.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Models
+{
+    internal class Weapon
+    {
+        private int _bulletCapacity;
+        private int _bulletCount;
+        private double _bulletTime;
+        public double ShootingTime { get; set; }
+        public ShootingMood FireMood { get; set; }
+
+
+        public int BulletCapacity
+        {
+            get
+            {
+                return _bulletCapacity;
+            }
+            set
+            {
+                if (value>0) 
+                { 
+                    _bulletCapacity = value;
+                }
+            }
+        }
+        public int BulletCount
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCount = value;
+                }
+            }
+        }
+        public double BulletTime
+        {
+            get
+            {
+                return _bulletTime;
+            }
+            set
+            {
+                if (value>0)
+                {
+                    _bulletTime = value;
+                }  
+            }
+        }
+
+        //    public ShootingMood ShootingMood { get; }
+
+        public Weapon(int bulletCapacity, int bulletCount, int bulletTime, ShootingMood FireMood)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            BulletTime = bulletTime;
+            ShootingMood shootingMood = FireMood;
+            ShootingTime = BulletTime / BulletCapacity;
+    }
+
+        public void Shoot()
+        {
+            BulletCapacity -= 1;
+            Console.WriteLine($"Bir gulle azaldi, qalan gulle sayi {BulletCount}");
+        }
+        public void Fire()
+        {
+            if (BulletCount!=0)
+            {
+                if (ShootingMood.single==FireMood)
+                {
+                    BulletCount-= 1;
+                }
+                else if (FireMood==ShootingMood.automatic)
+                {
+                    BulletCount = 0;
+                    Console.WriteLine(ShootingTime*BulletCount);
+                }
+            }
+            else
+            {
+                Console.WriteLine("sizin gulleniz yoxdur!");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity-BulletCount;
+        }
+
+        public void Relode()
+        {
+            if (BulletCount<BulletCapacity) 
+            {
+                BulletCount = BulletCapacity;            
+            }
+            else
+            {
+                Console.WriteLine("Daraq tam doludur!");
+            }
+        }
+        public void ChangeFireMood()
+        {
+            if (FireMood==ShootingMood.single)
+            {
+                FireMood = ShootingMood.automatic;
+            }
+            else
+            {
+                FireMood= ShootingMood.single;
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp.Models;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation;
+            Weapon weapon = new Weapon(100,50,60,ShootingMood.automatic);
+            
+            do
+            {
+                Console.WriteLine("0-Informasiya Almaq üçün");
+                Console.WriteLine("1-Shoot metodu üçün ");
+                Console.WriteLine("2-Fire Metodu üçün");
+                Console.WriteLine("3-Get Remain Bullet Count metodu üçün");
+                Console.WriteLine("4-Reload metodu üçün");
+                Console.WriteLine("5-Change Fire Mode metodu üçün");
+                Console.WriteLine("6-Proqramı dayandırmaq üçün");
+
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "0":
+                        Console.WriteLine();
+                        break;
+
+                    case "1":
+                        weapon.Shoot();
+                        break;
+
+                    case "2":
+                        weapon.Fire();
+                        break;
+
+                    case "3":
+                        weapon.GetRemainBulletCount();
+                        break;
+
+                    case "4":
+                        weapon.Relode();
+                        break;
+
+                    case "5":
+                        weapon.ChangeFireMood();
+                        break;
+                }
+            } while (operation != "6");
+        }
+    }
+}
